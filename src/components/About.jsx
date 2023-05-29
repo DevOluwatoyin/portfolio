a8962cb0e53bf4fd93f34f162ef3d7e19d789177
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2023-05-29 16:59:08 +0100

    Modify file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <div name="about" className="w-full bg-gradient-to-b from-gray-800 to-black text-white">
       <div className="max-w-7xl px-4 pt-24 mx-auto flex flex-col justify-center w-full md:px-8">
-        <div>
+        <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
         </div>
         <p className="text-xl">
