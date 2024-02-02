3fc4f0c3482da56283620fd0f4e940f897b8ad14
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2024-02-02 09:09:25 +0100

    added new projects

--- src/components/variables/projects.js
+++ src/components/variables/projects.js
@@ -1,31 +1,39 @@
-import paysequr from "../../assets/portfolio/paysequr.png";
+// import paysequr from "../../assets/portfolio/paysequr.png";
 import scissor from "../../assets/portfolio/scissor.png";
 import diginums from "../../assets/portfolio/diginums.png";
 import certified from "../../assets/portfolio/certified.png";
+import stryke from "../../assets/portfolio/stryke.png";
+import greenmind from "../../assets/portfolio/greenmind.png";
 
 export const projects = [
-  // {
-  //   id: 1,
-  //   name: "paysequr",
-  //   src: paysequr,
-  //   demo: "https://paysequr.com/",
-  // },
   {
-    id: 2,
-    name: "scissor",
-    src: scissor,
-    demo: "https://scissor-url-shortner.vercel.app/",
+    id: 1,
+    name: "certified",
+    src: certified,
+    demo: "https://certifieddump.com/",
   },
   {
-    id: 3,
+    id: 2,
     name: "diginums",
     src: diginums,
     demo: "https://diginums.com/",
   },
   {
+    id: 3,
+    name: "stryke",
+    src: stryke,
+    demo: "https://stryk.vercel.app/",
+  },
+  {
     id: 4,
-    name: "certified",
-    src: certified,
-    demo: "https://certifieddump.com/",
+    name: "greenmind",
+    src: greenmind,
+    demo: "https://greenemind.vercel.app/",
+  },
+  {
+    id: 5,
+    name: "scissor",
+    src: scissor,
+    demo: "https://scissor-url-shortner.vercel.app/",
   },
 ];
