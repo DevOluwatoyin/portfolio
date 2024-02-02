1f44b9e878f3f2d022747a38afe9343bd87ecd04
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2024-02-02 09:15:02 +0100

    added a new stack

--- src/components/variables/experience.jsx
+++ src/components/variables/experience.jsx
@@ -1,43 +1,66 @@
 export const tech = [
   {
     id: 1,
-    icon: <i className="devicon-html5-plain text-color-html text-7xl md:text-[150px]"></i>,
-    title: 'HTML',
-    style: "shadow-color-html"
+    icon: (
+      <i className="devicon-html5-plain text-color-html text-7xl md:text-[150px]"></i>
+    ),
+    title: "HTML",
+    style: "shadow-color-html",
   },
   {
     id: 2,
-    icon: <i className="devicon-css3-plain text-color-css text-7xl md:text-[150px]"></i>,
-    title: 'CSS',
-    style: "shadow-color-css"
+    icon: (
+      <i className="devicon-css3-plain text-color-css text-7xl md:text-[150px]"></i>
+    ),
+    title: "CSS",
+    style: "shadow-color-css",
   },
   {
     id: 3,
-    icon: <i className="devicon-javascript-plain text-color-javascript text-7xl md:text-[150px]"></i>,
-    title: 'Javascript',
-    style: "shadow-color-javascript"
+    icon: (
+      <i className="devicon-javascript-plain text-color-javascript text-7xl md:text-[150px]"></i>
+    ),
+    title: "Javascript",
+    style: "shadow-color-javascript",
   },
   {
     id: 4,
-    icon: <i className="devicon-react-original text-color-react text-7xl md:text-[150px]"></i>,
-    title: 'React',
-    style: "shadow-color-react"
+    icon: (
+      <i className="devicon-react-original text-color-react text-7xl md:text-[150px]"></i>
+    ),
+    title: "React",
+    style: "shadow-color-react",
   },
   {
     id: 5,
-    icon: <i className="devicon-tailwindcss-plain text-color-tailwind text-7xl md:text-[150px]"></i>,
-    title: 'Tailwind css',
-    style: "shadow-color-tailwind"
+    icon: (
+      <i className="devicon-tailwindcss-plain text-color-tailwind text-7xl md:text-[150px]"></i>
+    ),
+    title: "Tailwind css",
+    style: "shadow-color-tailwind",
   },
   {
     id: 6,
-    icon: <i className="devicon-github-original text-color-github text-7xl md:text-[150px]"></i>,
-    title: 'Github',
-    style: "shadow-color-github"
+    icon: (
+      <i className="devicon-github-original text-color-github text-7xl md:text-[150px]"></i>
+    ),
+    title: "Github",
+    style: "shadow-color-github",
   },
-    {id: 7,
-    icon: <i className="devicon-nextjs-plain text-white text-7xl md:text-[150px]"></i>,
-    title: 'Nextjs',
-    style: "shadow-white"
+  {
+    id: 7,
+    icon: (
+      <i className="devicon-nextjs-plain text-white text-7xl md:text-[150px]"></i>
+    ),
+    title: "Nextjs",
+    style: "shadow-white",
   },
-]
+  {
+    id: 8,
+    icon: (
+      <i className="devicon-typescript-plain text-blue-700 text-7xl md:text-[150px]"></i>
+    ),
+    title: "Typescript",
+    style: "shadow-react",
+  },
+];
