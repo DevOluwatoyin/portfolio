5128273139237da118c2ad8efcfc9b8ae3461eb4
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2025-03-05 15:01:48 +0100

    feat: add qwiva website to portfolio section

--- src/components/variables/projects.js
+++ src/components/variables/projects.js
@@ -4,8 +4,15 @@ import diginums from "../../assets/portfolio/diginums.png";
 import certified from "../../assets/portfolio/certified.png";
 import stryke from "../../assets/portfolio/stryke.png";
 import greenmind from "../../assets/portfolio/greenmind.png";
+import qwiva from "../../assets/portfolio/qwiva.png";
 
 export const projects = [
+  {
+    id: 7,
+    name: "qwiva",
+    src: qwiva,
+    demo: "https://qwiva.io/",
+  },
   {
     id: 1,
     name: "certified",
